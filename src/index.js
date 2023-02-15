7b01880b2dd34af071662cda5e97c1ee47039818
Sam Weeks <weekss@google.com>
2023-02-15 20:36:21 +0000

    init commit

--- src/index.js
+++ src/index.js
@@ -143,6 +143,7 @@ app.post('/*', async (req, res) => {
     for(var prop in req){
       console.log(prop);                      // Just get the property name only  
       console.log(result[prop]);   
+    }
   }
   if (!req.body) {
     const msg = 'no Pub/Sub message received';
